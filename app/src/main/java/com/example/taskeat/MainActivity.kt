47da8d1696c21343d62b86bbb78a93d44f9553cb
gonmarmar5 <80246529+gonmarmar5@users.noreply.github.com>
2023-03-09 17:37:52 +0100

    feat: splash screen added

--- app/src/main/java/com/example/taskeat/MainActivity.kt
+++ app/src/main/java/com/example/taskeat/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.taskeat
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import java.security.Principal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_sign_in)
     }
+
+
 }
