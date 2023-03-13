f6905834b0ce9c3d12b2dd6edaff716af5f7d46d
gonmarmar5 <80246529+gonmarmar5@users.noreply.github.com>
2023-03-13 18:21:01 +0100

    feat: Connection with Firestore and refactorized app name

--- app/src/main/java/com/example/taskeat/MainActivity.kt
+++ app/src/main/java/com/example/taskeat/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.taskeat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import android.util.Log
+import android.content.ContentValues.TAG
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val db = Firebase.firestore
+
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Trucker",
+            "last" to "Mariquita",
+            "born" to 2023
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
